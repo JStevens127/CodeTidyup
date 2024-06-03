da14adc1efc255ac8ae15a4e1b610eeb07ce1553
Jack <jack.p.stevens@gmail.com>
2024-06-03 00:29:54 -0400

    Add icon library

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,13 @@
-import { AppIcon } from "@/components/icons";
-import Image from "next/image";
+import { AppIcon } from '@/components/icons'
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="flex flex-col gap-y-2 items-center">
-        <AppIcon className="w-24 h-24" />
-        <div className="font-bold">A better online code formatter</div>
-        <div>Launching shortly...</div>
-      </div>
-    </main>
-  );
+    return (
+        <main className='flex min-h-screen flex-col items-center justify-between p-24'>
+            <div className='flex flex-col gap-y-2 items-center'>
+                <AppIcon className='w-24 h-24' />
+                <div className='font-bold text-xl'>A better online code formatter</div>
+                <div>Launching shortly...</div>
+            </div>
+        </main>
+    )
 }
