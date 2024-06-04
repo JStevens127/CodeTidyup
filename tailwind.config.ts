17fe40e1e4c461f6dbb0a5048991d10d2f178ae4
Jack <jack.p.stevens@gmail.com>
2024-06-03 23:35:04 -0400

    Styling adjustments

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,31 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
 
 const config: Config = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+    content: [
+        './pages/**/*.{js,ts,jsx,tsx,mdx}',
+        './components/**/*.{js,ts,jsx,tsx,mdx}',
+        './app/**/*.{js,ts,jsx,tsx,mdx}'
+    ],
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['Inter', 'ui-sans-serif', 'system-ui'],
+                serif: ['ui-serif', 'Georgia'],
+                mono: ['ui-monospace', 'SFMono-Regular']
+            },
+            backgroundImage: {
+                'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+                'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))'
+            },
+            colors: {
+                textPrimary: 'rgba(0, 0, 0, 0.87)',
+                textSecondary: 'rgba(0, 0, 0, 0.6)',
+                textDisabled: 'rgba(0, 0, 0, 0.38)',
+                divider: 'rgba(0, 0, 0, 0.08)',
+                hover: 'rgba(0, 0, 0, 0.04)'
+            }
+        }
     },
-  },
-  plugins: [],
-};
-export default config;
+    plugins: []
+}
+export default config
